78542e3a14575c993040385e379f683af537be62
DivyaShanmugam <divya.shanmugam@capgemini.com>
2016-07-07 10:03:08 +0530

    Initial commit

---
+++ ProductApp/src/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+	public static void main(String[] args) {
+		ProductApp app=new ProductApp();
+		app.productDisplay();
+		greetings();
+	}
+	public static void greetings()
+	{
+		System.out.println("Divya");
+		System.out.println("Sharath");
+	}
+}
---
+++ ProductApp/src/org/cap/demo/ProductApp.java
@@ -0,0 +1,10 @@
+package org.cap.demo;
+
+public class ProductApp {
+
+	public void productDisplay() {
+		// TODO Auto-generated method stub
+		System.out.println("Capgemini");
+	}
+
+}
