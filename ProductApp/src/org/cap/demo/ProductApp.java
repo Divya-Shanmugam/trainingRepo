81490dd953949e5e2ef58662a845e274ddff3051
DivyaShanmugam <divya.shanmugam@capgemini.com>
2016-07-07 10:22:12 +0530

    Change Display

--- ProductApp/src/org/cap/demo/ProductApp.java
+++ ProductApp/src/org/cap/demo/ProductApp.java
@@ -2,9 +2,15 @@ package org.cap.demo;
 
 public class ProductApp {
 
+	int[] arr={1,0,34};
 	public void productDisplay() {
 		// TODO Auto-generated method stub
-		System.out.println("Capgemini");
+
+
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Data:");
+		}
 	}
 
 }
